84605c6dd5d76331c82bc921d8b4e18b4331e68f
BehkoVasilev <vencislavvsilev@gmail.com>
2023-01-22 22:50:39 +0200

    add routers and cube controller

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ const express = require('express');
 const config = require('./config/config');
 const setupViewEngine = require('./config/viewEngine');
 // require('./config/viewEngine')(app);
+const router = require('./routes');
 
 const app = express();
 setupViewEngine(app);
 
-app.use(express.static("src/public"))
-
-app.get('/', (req, res) => {
-    res.render("index")
-});
-
-app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}`))
+app.use(express.static("src/public"));
+app.use(router)
+app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}`));
