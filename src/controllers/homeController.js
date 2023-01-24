7dfa06382c648078a24e2852b0e8360d2fc31986
BehkoVasilev <vencislavvsilev@gmail.com>
2023-01-24 16:03:49 +0200

    add search implementation

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,25 @@
 const db = require('../db.json')
 
 exports.getHomeController = (req, res) => {
-    console.log(db.cubes);
-    res.render("index", {cubes: db.cubes})
+    let { search, from, to } = req.query;
+    from = Number(from);
+    to = Number(to);
+
+    let cubes = db.cubes;
+
+    if (search) {
+        cubes = cubes.filter(cube => cube.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
+    };
+
+    if (from) {
+        cubes = cubes.filter(cube => cube.difficultyLevel >= from);
+    };
+
+    if (to) {
+        cubes = cubes.filter(cube => cube.difficultyLevel <= to);
+    };
+
+    res.render("index", { cubes, search, from, to })
 };
 
 exports.getAboutController = (req, res) => {
