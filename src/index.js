fbc267d4e3a3a62695a2b9776d76355229d43b35
BehkoVasilev <vencislavvsilev@gmail.com>
2023-02-11 22:23:20 +0200

    add cookie-parser and jwt

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
+
 const config = require('./config/config');
 const initDatabase = require('./config/initDatabase');
 const setupViewEngine = require('./config/viewEngine');
 // require('./config/viewEngine')(app);
-const router = require('./routes');
+const routes = require('./routes');
 
 const app = express();
 setupViewEngine(app);
 
 app.use(express.static("src/public"));
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }))
-app.use(router);
+app.use(routes);
 
 initDatabase()
     .then(() => app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}`)))
